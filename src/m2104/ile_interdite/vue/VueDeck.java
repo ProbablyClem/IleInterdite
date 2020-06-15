2ad9fa5214e0b03c0dfebc3095565470e592db42
ProbablyClem <45786362+ProbablyClem@users.noreply.github.com>
2020-06-15 16:53:37 +0200

    Vue Deck

--- src/m2104/ile_interdite/vue/VueDeck.java
+++ src/m2104/ile_interdite/vue/VueDeck.java
@@ -1,58 +1,60 @@
 package m2104.ile_interdite.vue;
 
-import m2104.ile_interdite.modele.IleInterdite;
+import m2104.ile_interdite.modele.Carte;
+import m2104.ile_interdite.modele.CarteTresor;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
-public abstract class VueDeck extends JPanel {
+public class VueDeck extends JPanel {
     private IHM vueIHM;
-    private JLabel nomdeck;
+    private JLabel labelNomDeck;
     private JButton retour;
-    private JPanel mainpanel;
+    private JPanel grid;
     private JPanel panelhaut;
-    private JPanel cardisplay;
-    private IleInterdite iledeck;
+    private ArrayList<Carte> cartes;
 
-    public IleInterdite getIledeck() {
-        return iledeck;
-    }
 
-    public VueDeck() {
-        nomdeck= new JLabel("FOSSE");
+    public VueDeck(String nomDeck, ArrayList<Carte> cartes) {
+        this.cartes = cartes;
+        this.setLayout(new BorderLayout());
+
+        panelhaut= new JPanel(new BorderLayout());
+
         retour= new JButton("RETOUR");
         retour.setBackground(Color.RED);
-        panelhaut= new JPanel();
-        mainpanel= new JPanel();
-        cardisplay=new JPanel();
-        retour=new JButton();
-        panelhaut.add(retour);
-        panelhaut.add(nomdeck);
-        panelhaut.add(retour);
-        this.setLayout(new BorderLayout());
-        this.add(panelhaut,BorderLayout.NORTH);
-        this.add(cardisplay);
-        this.add(mainpanel);
-    }
+        panelhaut.add(retour, BorderLayout.WEST);
+        labelNomDeck = new JLabel(nomDeck);
+        panelhaut.add(labelNomDeck, BorderLayout.CENTER);
 
-    public JLabel getNomdeck() {
-        return nomdeck;
+        this.add(panelhaut,BorderLayout.NORTH);
+
+        if (cartes.size() <= 14){
+            grid = new JPanel(new GridLayout(1, 14));
+        }
+        else {
+            grid = new JPanel(new GridLayout(2, 14));
+        }
+
+        for(Carte c : cartes){
+            grid.add(c.getImage());
+        }
+        this.add(grid, BorderLayout.CENTER);
     }
 
     public static void main(String[] args) {
-         VueCartesInondation vueDeck= new VueCartesInondation();
+        ArrayList<Carte> c = new ArrayList<>();
+        CarteTresor carte = new CarteTresor("src/images/ocean.jpg", "Carte", null);
+        CarteTresor cartee = new CarteTresor("src/images/ocean.jpg", "Carte", null);
+        c.add(carte);
+        c.add(cartee);
+         VueDeck vueDeck= new VueDeck("Deck", c);
          JFrame w = new JFrame();
          w.add(vueDeck);
+         w.setSize(500, 300);
          w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
          w.setTitle("Vueflex");
          w.setVisible(true);
-    }
-    public abstract void decksize(JPanel cardisplay);
-    public abstract void deckname(JLabel nomdeck);
-
-    public void setNomdeck(JLabel nomdeck) {
-        this.nomdeck = nomdeck;
     }
 }
