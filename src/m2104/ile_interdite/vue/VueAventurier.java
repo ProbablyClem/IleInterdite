e9f6d6945fdd5a75761867224fbe5f5127c3563f
ProbablyClem <45786362+ProbablyClem@users.noreply.github.com>
2020-06-15 19:00:47 +0200

    Boutons Vue Aventurier

--- src/m2104/ile_interdite/vue/VueAventurier.java
+++ src/m2104/ile_interdite/vue/VueAventurier.java
@@ -7,12 +7,14 @@ import m2104.ile_interdite.modele.Messager;
 import m2104.ile_interdite.util.Utils;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class VueAventurier extends JPanel implements ActionListener {
     private IleInterdite ileInterdite;
+    private IHM ihm;
     private Aventurier aventurier;
     private JLabel nomAventurier;
     private JLabel nbActionsLabel;
@@ -29,18 +31,21 @@ public class VueAventurier extends JPanel implements ActionListener {
     private JButton cartesInondation;
     private int nbActions;
 
-    public VueAventurier(Aventurier aventurier, IleInterdite ileInterdite){
+    public VueAventurier(Aventurier aventurier, IleInterdite ileInterdite, IHM ihm){
         this.ileInterdite = ileInterdite;
         this.aventurier = aventurier;
+        this.ihm = ihm;
 
         this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         JPanel header = new JPanel(new BorderLayout());
 
-        nomAventurier = new JLabel(aventurier.getNom());
-        header.add(nomAventurier, BorderLayout.WEST);
+        nomAventurier = new JLabel(aventurier.getNom() + " : " + aventurier.getRole());
+        nomAventurier.setFont(new Font(nomAventurier.getFont().getFontName(), Font.PLAIN, 30));
+        header.add(nomAventurier, BorderLayout.CENTER);
 
-        nbActionsLabel = new JLabel(nbActions + " /3");
+        nbActionsLabel = new JLabel("Actions restantes : " + nbActions + " /3");
+        nbActionsLabel.setFont(new Font(nbActionsLabel.getFont().getFontName(), Font.PLAIN, 27));
         header.add(nbActionsLabel, BorderLayout.EAST);
 
         this.add(header);
@@ -48,23 +53,35 @@ public class VueAventurier extends JPanel implements ActionListener {
         textField = new JLabel("Choisir une action");
         this.add(textField);
 
-        JPanel actionsPanel = new JPanel(new GridLayout(2, 3));
+        JPanel actionsPanel = new JPanel(new GridLayout(2, 3, 20, 20));
         seDeplacer = new JButton("Se Deplacer");
+        seDeplacer.setBorder(new RoundedBorder(20));
+        seDeplacer.addActionListener(this);
         actionsPanel.add(seDeplacer);
 
         assecher = new JButton("Assecher");
+        assecher.setBorder(new RoundedBorder(20));
+        assecher.addActionListener(this);
         actionsPanel.add(assecher);
 
         finirTour = new JButton("Finir Tour");
+        finirTour.setBorder(new RoundedBorder(20));
+        finirTour.addActionListener(this);
         actionsPanel.add(finirTour);
 
         actionSpecial = new JButton("Action Special");
+        actionSpecial.setBorder(new RoundedBorder(20));
+        actionSpecial.addActionListener(this);
         actionsPanel.add(actionSpecial);
 
         donnerCarte = new JButton("Donner Carte");
+        donnerCarte.setBorder(new RoundedBorder(20));
+        donnerCarte.addActionListener(this);
         actionsPanel.add(donnerCarte);
 
         prendreTresor = new JButton("Prendre Tresor");
+        prendreTresor.setBorder(new RoundedBorder(20));
+        prendreTresor.addActionListener(this);
         actionsPanel.add(prendreTresor);
 
         this.add(actionsPanel);
@@ -72,20 +89,24 @@ public class VueAventurier extends JPanel implements ActionListener {
         VueMain main = new VueMain(aventurier.getCartes());
         this.add(main);
 
-        JPanel decks = new JPanel(new GridLayout(0, 4));
+        JPanel decks = new JPanel(new GridLayout(0, 4, 20, 20));
         cartesTresor = new JButton("Carte Tresor");
+        cartesTresor.setBorder(new RoundedBorder(20));
         cartesTresor.addActionListener(this);
         decks.add(cartesTresor);
 
         deffauseeTresor = new JButton("Deffausse Carte Tresor");
+        deffauseeTresor.setBorder(new RoundedBorder(20));
         deffauseeTresor.addActionListener(this);
         decks.add(deffauseeTresor);
 
         deffauseInondation = new JButton("Deffausse Carte Innondation");
+        deffauseInondation.setBorder(new RoundedBorder(20));
         deffauseInondation.addActionListener(this);
         decks.add(deffauseInondation);
 
         cartesInondation = new JButton("Carte Inondation");
+        cartesInondation.setBorder(new RoundedBorder(20));
         cartesInondation.addActionListener(this);
         decks.add(cartesInondation);
 
@@ -103,7 +124,7 @@ public class VueAventurier extends JPanel implements ActionListener {
         aventurier.donnerCarte(new CarteTresor("src/images/ocean.jpg", ".", Utils.Tresor.CALICE));
         aventurier.donnerCarte(new CarteTresor("src/images/ocean.jpg", ".", Utils.Tresor.CALICE));
         aventurier.donnerCarte(new CarteTresor("src/images/ocean.jpg", ".", Utils.Tresor.CALICE));
-        VueAventurier v = new VueAventurier(aventurier, null);
+        VueAventurier v = new VueAventurier(aventurier, null, null);
         JFrame w = new JFrame();
         w.add(v);
         w.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -129,6 +150,42 @@ public class VueAventurier extends JPanel implements ActionListener {
         else if (e.getSource() == cartesInondation){
             VueDeck v = new VueDeck("Deck Cartes Inondation", ileInterdite.getDeckTresor());
             v.afficher();
+        }
+        else if(e.getSource() == seDeplacer){
+
+        }
+        else if(e.getSource() == assecher){
+
+        }
+        else if(e.getSource() == finirTour){
+
+        }
+        else if(e.getSource() == actionSpecial){
+
+        }
+        else if(e.getSource() == donnerCarte){
+
+        }
+        else if(e.getSource() == prendreTresor){
+
+        }
+    }
+
+    private static class RoundedBorder implements Border {
+        private int radius;
+        RoundedBorder(int radius) {
+            this.radius = radius;
+        }
+
+        public Insets getBorderInsets(Component c) {
+            return new Insets(this.radius+1, this.radius+1, this.radius+2, this.radius);
+        }
+        public boolean isBorderOpaque() {
+            return true;
+        }
+
+        public void paintBorder(Component c, Graphics g, int x, int y, int width, int height) {
+            g.drawRoundRect(x, y, width-1, height-1, radius, radius);
         }
     }
 }
