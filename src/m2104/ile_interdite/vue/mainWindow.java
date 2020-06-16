9982749bd6c91779660aaeabb20016436c77428d
Thomas Duplessis <thomas.duplessis@etu.univ-grenoble-alpes.fr>
2020-06-16 18:10:49 +0200

    VueInscription + centrage fenetres

--- src/m2104/ile_interdite/vue/mainWindow.java
+++ src/m2104/ile_interdite/vue/mainWindow.java
@@ -17,6 +17,7 @@ public class mainWindow extends JFrame{
     private Grille grille;
 
     public mainWindow(IHM ihm, Grille grille, VueAventurier v){
+
         this.grille = grille;
         this.ihm = ihm;
 
@@ -24,7 +25,7 @@ public class mainWindow extends JFrame{
         this.niveauPanel = new JPanel(new BorderLayout());
         this.vueNiveau = new VueNiveau(2);
         this.niveauPanel.add(vueNiveau, BorderLayout.SOUTH);
-        JButton exit = new JButton("Quitter");
+        JButton exit = new Button("Quitter", 0, 35, Color.RED, new Color(255, 120, 120));
         exit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -42,7 +43,7 @@ public class mainWindow extends JFrame{
 
         this.setContentPane(mainPanel);
         this.setSize(1750, 800);
-        this.setLocation(400, 150);
+        this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
 
